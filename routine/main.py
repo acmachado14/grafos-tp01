2c13b5c63f4198b29db43e62658a13688c389059
Angelo <cupertinoangelo13@gmail.com>
2022-10-10 21:22:26 -0300

    :sparkles: feat: Json to txt

--- routine/main.py
+++ routine/main.py
@@ -1,4 +1,19 @@
-def main():
-    print("initial")
+import json
 
-main()
+with open("grafos-json/Grafo01.json", encoding='utf-8') as meu_json:
+    dados = json.load(meu_json)
+
+arquivo = open('grafos-txt/grafo.txt','w')
+
+qtdNodes = len(dados['data']['nodes']['_data'])
+
+arquivo.write(f"{qtdNodes} \n")
+
+for i in dados['data']['edges']['_data']:
+    data = dados['data']['edges']['_data'][str(i)]
+    if 'label' in data:
+        arquivo.write(f"{data['from']} {data['to']} {data['label']} \n")
+    else:
+        arquivo.write(f"{data['from']} {data['to']} {1} \n")
+
+arquivo.close()
