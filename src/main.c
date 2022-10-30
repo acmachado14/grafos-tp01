f7ea242a6a48ae27f7978e68db809839791073d3
Arthur Bastos <afbastos8@gmail.com>
2022-10-30 19:52:01 -0300

    Adicionar terminal interativo

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@ int main(){
     Grafo grafo;
     char nomeArquivo[100];
     bool leituraOK;
+    int vizinhoDe,grauDe,profundidadeDe,exenDe,centralDe,distanciaDe,distanciaPara,caminhoDe,caminhoPara;
     printf("Digite o nome do arquivo a ser executado: ");
     scanf("%s",&nomeArquivo);
     printf("\n");
@@ -13,20 +14,48 @@ int main(){
         return 0;
     }
     printf("\n");
+
+    printf("Digite o vertice para descobrir seus vizinhos\n");
+    scanf("%d",&vizinhoDe);
+    printf("Digite o vertice para descobrir seu grau\n");
+    scanf("%d",&grauDe);
+    printf("Digite o vertice de inicio da busca de profundidade\n");
+    scanf("%d",&profundidadeDe);
+    printf("Digite o vertice para descobrir sua Exentricidade\n");
+    scanf("%d",&exenDe);
+    printf("Digite o vertice para descobrir a Centralidade de Proximidade\n");
+    scanf("%d",&centralDe);
+    printf("Digite os dois vertices para descobrir a distancia entre eles\n");
+    scanf("%d",&distanciaDe);
+    scanf("%d",&distanciaPara);
+    printf("Digite os vertices para descobrir a distancia entre eles\n");
+    scanf("%d",&caminhoDe);
+    scanf("%d",&caminhoPara);
+
     printf("Ordem grafo: %d\n", ordemGrafo(&grafo));
+
     printf("Tamanho grafo: %d\n", tamanhoGrafo(&grafo));
-    printf("Vertices vizinhos do 5: %s\n", verticesVizinhos(&grafo, 5)); // exemplo
-    printf("Grau do vertice 5: %d\n", verticeGrau(&grafo, 5));
+
+    printf("Vertices vizinhos do %d: %s\n",vizinhoDe,verticesVizinhos(&grafo,vizinhoDe));
+    
+    printf("Grau do vertice %d: %d\n",grauDe,verticeGrau(&grafo, grauDe));
+    
     printf("Sequencia de graus: %s\n", sequenciaGrausGrafo(&grafo));
-    printf("Busca de Profundidade: \n");
-    buscaProfundidade(&grafo,1);
-    printf("Excentricidade do vertice 1: %f\n", excentricidadeVertice(&grafo, 1));
+    
+    printf("Busca de Profundidade a partir de %d: \n",profundidadeDe);
+    buscaProfundidade(&grafo,profundidadeDe);
+
+    printf("Excentricidade do vertice %d: %f\n",exenDe, excentricidadeVertice(&grafo,exenDe));
+
     printf("Raio grafo: %f\n", raioGrafo(&grafo));
     printf("Diametro grafo: %f\n", diametroGrafo(&grafo));
     printf("Centro grafo: %s\n", centroGrafo(&grafo));
-    printf("Centralidade proximidade do vertice 5: %f\n", centralidadeProximidade(&grafo, 5));
-    printf("Distancia entre vertice: %f\n", distanciaEntreVertice(&grafo, 1, 7));
-    printf("Caminho: %s\n", caminhoMinimoEntreVertice(&grafo, 1, 9));
+
+    printf("Centralidade proximidade do vertice %d: %f\n",centralDe, centralidadeProximidade(&grafo,centralDe));
+
+    printf("Distancia entre vertice %d e %d: %f\n",distanciaDe,distanciaPara,distanciaEntreVertice(&grafo,distanciaDe,distanciaPara));
+
+    printf("Caminho de %d para %d: %s\n",caminhoDe,caminhoPara, caminhoMinimoEntreVertice(&grafo,caminhoDe,caminhoPara));
     printf("\n");
     return 0;
 }
